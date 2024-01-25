d36f401790605748b568b5aa19ef03c9affefd95
Tom Bremer <tom@tbremer.com>
2024-01-25 10:29:14 -0600

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -54,7 +54,8 @@ export const actions: Actions = {
 		cookies.set('dob', formData.birthDay, getCookieOptions());
 		cookies.set('sex', formData.sex, getCookieOptions());
 
-		const percent = (zScoreToPercent(zScore, false) * 100).toFixed(2);
+		// const percent = (zScoreToPercent(zScore, true) * 100).toFixed(2);
+		const percent = (zScoreToPercent(zScore)).toFixed(2);
 
 		return {
 			monthsOld,
